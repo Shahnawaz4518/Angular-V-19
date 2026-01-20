82b093ca203bed3feae57d6346268f2b4c190a9c
Shahnawaz4518 <shahnawaz6499@gmail.com>
2026-01-20 16:30:46 +0530

    Reactiveform-2

--- myangularapp_f14/src/app/reactiveform/reactiveform.ts
+++ myangularapp_f14/src/app/reactiveform/reactiveform.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 //import { ReactiveFormsModule } from '@angular/forms';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, ReactiveFormsModule, FormBuilder } from '@angular/forms';
 
 
 
@@ -14,23 +14,50 @@ export class Reactiveform {
 
 regForm!: FormGroup;
 
-constructor(){}
+constructor(private _fb:FormBuilder){}
+
+// ngOnInit(){
+//   this.regForm=new FormGroup({
+//       id:new FormControl(),
+//       fname:new FormControl(),
+//       lname:new FormControl(),
+//       email:new FormControl(),
+//       mobileno:new FormControl(),
+
+      
+//   })
+// }
+
+
+// ngOnInit(){
+//   this.regForm=this._fb.group({
+//       id:new FormControl(''),
+//       fname:new FormControl(''),
+//       lname:new FormControl(''),
+//       email:new FormControl(''),
+//       mobileno:new FormControl('1234567891'),
+
+      
+//   })
+// }
+
 
 ngOnInit(){
-  this.regForm=new FormGroup({
-      id:new FormControl(),
-      fname:new FormControl(),
-      lname:new FormControl(),
-      email:new FormControl(),
-      mobileno:new FormControl(),
+  this.regForm=this._fb.group({
+      id:[''],
+      fname:[''],
+      lname:[''],
+      email:[''],
+      mobileno:[''],
 
       
   })
 }
 
 register(formdata:FormGroup){
-  console.log(formdata.value)
-
+  //console.log(formdata.value)
+   console.log(this.regForm.value);
+   console.log(formdata.valid)
 }
 
 }
