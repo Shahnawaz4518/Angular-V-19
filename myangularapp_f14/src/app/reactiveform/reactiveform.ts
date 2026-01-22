738a469d3703cb33adaba7753b30cade9847be42
Shahnawaz4518 <shahnawaz6499@gmail.com>
2026-01-22 15:34:24 +0530

    Reactive Form With Validators and get,patch,set value from reactive form and valueChanges and statusChanges, added FormArray with Dynamic Control In FormArray

--- myangularapp_f14/src/app/reactiveform/reactiveform.ts
+++ myangularapp_f14/src/app/reactiveform/reactiveform.ts
@@ -3,53 +3,26 @@ import { Component } from '@angular/core';
 //import { ReactiveFormsModule } from '@angular/forms';
 import { FormGroup, FormControl, ReactiveFormsModule, FormBuilder, Validators, FormArray } from '@angular/forms';
 
-
-
 @Component({
   selector: 'app-reactiveform',
-  imports: [ReactiveFormsModule,CommonModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './reactiveform.html',
   styleUrl: './reactiveform.css',
 })
 export class Reactiveform {
 
-regForm!: FormGroup;
+  regForm!: any;
 
-constructor(private _fb:FormBuilder){}
+  constructor(private _fb: FormBuilder) {}
 
-// ngOnInit(){
-//   this.regForm=new FormGroup({
-//       id:new FormControl(),
-//       fname:new FormControl(),
-//       lname:new FormControl(),
-//       email:new FormControl(),
-//       mobileno:new FormControl(),
-
-      
-//   })
-// }
-
-
-// ngOnInit(){
-//   this.regForm=this._fb.group({
-//       id:new FormControl(''),
-//       fname:new FormControl(''),
-//       lname:new FormControl(''),
-//       email:new FormControl(''),
-//       mobileno:new FormControl('1234567891'),
-
-      
-//   })
-// }
-
-
-ngOnInit() {
+  ngOnInit() {
     this.regForm = this._fb.group({
       id: [''],
 
       fname: ['', [
         Validators.required,
-        Validators.minLength(10)]],
+        Validators.minLength(10)
+      ]],
 
       lname: ['', Validators.required],
 
@@ -61,7 +34,14 @@ ngOnInit() {
       mobileno: ['', [
         Validators.required,
         Validators.pattern('^[0-9]{10}$')
-      ]]
+      ]],
+
+      // ===============================
+      // Dynamic Control in FormArray In Reactive Form
+      // ===============================
+      skills: this._fb.array([
+        new FormControl('', Validators.required)
+      ])
     });
 
     // this.regForm.get('fname')?.valueChanges.subscribe(firstname =>{
@@ -72,108 +52,59 @@ ngOnInit() {
     //   console.log('lname value'+lastname)
     // })
 
-
-    // if we cant to apply whole form then we use
     // this.regForm.valueChanges.subscribe(formdata =>{
     //   console.log("fname value : "+formdata.fname);
     //   console.log("lname value : "+formdata.lname);
-      
     // })
-
-
-
-    // for status is valid or not
-    // this.regForm.get('fname')?.statusChanges.subscribe(firstnamestatus => {
-    //   console.log("fname status : "+firstnamestatus);
-    // })
-
-    // this.regForm.get('lname')?.statusChanges.subscribe(lastnamestatus => {
-    //   console.log("lname status : "+lastnamestatus);
-    // })
-
-
-
-    //for whole form we use this
 
     // this.regForm.statusChanges.subscribe(formdata => {
     //   console.log("form status : "+formdata);
     // })
+  }
 
+  // ===============================
+  // Getter for FormArray
+  // ===============================
+  get skills(): FormArray {
+    return this.regForm.get('skills') as FormArray;
+  }
 
+  // ===============================
+  // Add Dynamic Control
+  // ===============================
+  addSkill() {
+    this.skills.push(
+      new FormControl('', Validators.required)
+    );
+  }
 
-
-    //FormArray In Reactive Form
-    // let arr=new FormArray([
-    //   new FormControl(),
-    //   new FormControl()
-    // ])
-    
-    // console.log(arr.value);
-    // console.log(arr.valid);
-
-
-
-
-    //   let arr=new FormArray([
-    //   new FormControl('Shahnawaz'),
-    //   new FormControl('1234567891')
-    // ])
-    
-    // console.log(arr.value);
-    // console.log(arr.valid);
-
-
-    // now add validators
-
-      let arr=new FormArray([
-      new FormControl('Shahnawaz'),
-      new FormControl('',Validators.required)
-    ])
-    
-    console.log(arr.value);
-    console.log(arr.valid);
-
-    arr.reset();
-
-    console.log(arr.value);
-    console.log(arr.valid);
-
+  // ===============================
+  // Remove Dynamic Control
+  // ===============================
+  removeSkill(index: number) {
+    this.skills.removeAt(index);
   }
 
   register(formdata: FormGroup) {
     console.log(this.regForm.value);
     console.log(formdata.valid);
 
-    //get value
     console.log(this.regForm.get('fname')?.value);
     console.log(this.regForm.get('email')?.value);
   }
 
-  reset(){
-    //this.regForm.reset();
-
-
-    //static way to get data particaaly received and reset
-    // this.regForm.reset({
-    //   fname:'Virat'
-    // })
-
-
-    //dynamic way to get data partially received and reset
-    this.regForm.reset({
-      
-    })
+  reset() {
+    this.regForm.reset({});
   }
 
-
-  // if we need fill full data on form we use set value and if we want fill partically we use patched value
-  filldata(){
+  filldata() {
     this.regForm.setValue({
-      id:101,
-      fname:'ViratKohli',
-      lname:'Kohli',
-      email:'virat@gmail.com',
-      mobileno:'1234567891'
-    })
+      id: 101,
+      fname: 'ViratKohli',
+      lname: 'Kohli',
+      email: 'virat@gmail.com',
+      mobileno: '1234567891',
+      skills: ['Angular']
+    });
   }
 }
