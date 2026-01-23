9b77c9174c72546609967ce9b6cf18cc87667582
Shahnawaz4518 <shahnawaz6499@gmail.com>
2026-01-23 16:23:53 +0530

    Simple Counter App

--- myangularapp_f16/src/app/app.ts
+++ myangularapp_f16/src/app/app.ts
@@ -9,4 +9,42 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('myangularapp_f16');
+
+  count:number=0;
+
+
+  // Below Using 3 Methods Counter App
+
+
+
+  // increment (){
+  //   this.count=this.count+1;
+  // }
+
+  // reset (){
+  //   this.count=0;
+  // }
+
+  // decrement (){
+  //   this.count=this.count-1;
+  // }
+
+
+
+
+
+  // Below Using Single Method Counter App 
+
+  onchange(val:any){
+    if(val=='plus'){
+      this.count=this.count+1;
+    }else if(val=='minus'){
+      this.count=this.count-1;
+    }else{
+      this.count=0;
+    }
+  }
+
+
+
 }
