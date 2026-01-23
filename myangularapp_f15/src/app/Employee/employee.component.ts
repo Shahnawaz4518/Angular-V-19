82ef20ae6c264ae447cbbc3c380f7f1a8a75206a
Shahnawaz4518 <shahnawaz6499@gmail.com>
2026-01-23 12:02:13 +0530

    Creating Custom Component Without CLI

---
+++ myangularapp_f15/src/app/Employee/employee.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-employee',
+    templateUrl:'./employee.component.html',
+// template:'<h1>Hello Creating Componenet Without CLI</h1>'
+// styles:['h1{background-color:red}']
+    styleUrl:'./employee.component.css'
+})
+
+
+export class empoloyee{
+
+}
--- myangularapp_f15/src/app/app.ts
+++ myangularapp_f15/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Student } from "./student/student";
+import { empoloyee } from "./Employee/employee.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Student, empoloyee],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
